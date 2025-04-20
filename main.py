4c760f9ae9d24fdb5f2f478719b2c0abb9a3dce1
Fred <64082311+MatiP911@users.noreply.github.com>
2025-04-20 17:03:28 +0200

    tideUp stopper

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import customtkinter as ctk
 
+windowSize = (300, 230)
+stopperFontSize = 100
+
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("600x300")
+        self.geometry(f"{windowSize[0]}x{windowSize[1]}")
         self.title("Clock")
+        self.resizable(False, False)
 
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=2, uniform='a')
@@ -13,7 +17,8 @@ class App(ctk.CTk):
         self.textVar = ctk.StringVar(value="")
 
         self.clock = ctk.CTkLabel(
-            self, textvariable=self.textVar)
+            self, textvariable=self.textVar,
+            font=ctk.CTkFont(size=stopperFontSize))
         self.clock.bind("<Button-1>", self.resetTime)
         self.clock.grid(row=0, column=0, sticky="nswe")
 
