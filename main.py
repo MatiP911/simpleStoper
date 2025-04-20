f09cb9bc315cd230a0c20219fa655a213d084ef9
Fred <64082311+MatiP911@users.noreply.github.com>
2025-04-20 16:25:35 +0200

    just clock

---
+++ main.py
@@ -0,0 +1,37 @@
+import customtkinter as ctk
+import time
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("600x300")
+        self.title("Clock")
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=2, uniform='a')
+
+        self.textVar = ctk.StringVar(value="")
+
+        self.clock = ctk.CTkLabel(self, textvariable=self.textVar)
+        self.clock.grid(row=0, column=0, sticky="nswe")
+
+        self.time()
+
+        self.mainloop()
+
+    def time(self):
+        aktualny_czas = time.strftime("%H:%M:%S")
+        self.textVar.set(aktualny_czas)
+
+        self.after(1000, self.time)
+
+
+class Clock(ctk.CTkLabel):
+    def __init__(self, parent, text):
+        super().__init__(parent)
+        self.configure(textvariable=text)
+
+
+if __name__ == '__main__':
+    App()
